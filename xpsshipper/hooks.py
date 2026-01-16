601da35bc48b7c928476cce23c1d301d2904caa0
KorucuTech <kemal@korucutech.com>
2026-01-15 21:56:06 -0500

    Fix Error

--- xpsshipper/hooks.py
+++ xpsshipper/hooks.py
@@ -261,11 +261,11 @@ fixtures = [
         "filters": {
             "module": "XPSShipper",
             }
-    },
-    {
-        "dt": "Workspace",
-        "filters": {
-            "module": "XPSShipper",
-            }
     }
+    # {
+    #     "dt": "Workspace",
+    #     "filters": {
+    #         "module": "XPSShipper",
+    #         }
+    # }
 ]
