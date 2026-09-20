197f29c9ae0eb42aa7dd987034834f887a4c38cb
KorucuTech <kemal@korucutech.com>
2026-09-20 14:49:36 -0400

    Chnage Column Size

--- xpsshipper/xpsshipper/report/order_tracking_3/order_tracking_3.py
+++ xpsshipper/xpsshipper/report/order_tracking_3/order_tracking_3.py
@@ -63,7 +63,7 @@ def get_columns():
             "label": "Order Status",
             "fieldname": "order_status",
             "fieldtype": "Data",
-            "width": 150
+            "width": 200
         },
         {
             "label": "Invoices",
